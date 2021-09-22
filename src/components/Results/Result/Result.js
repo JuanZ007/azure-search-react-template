9739de5186631b55293bee4db310013891c187f3
JuanZ007 <32806138+JuanZ007@users.noreply.github.com>
2021-09-21 21:42:36 -0500

    Update Result.js

--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -6,10 +6,6 @@ export default function Result(props) {
     return(
 	<a href="http://{`${props.document.location}`}" target="_blank">props.document.text</a>
 
-	console.log(props.document);
-	console.log(props.document.text);
-	console.log(props.document.location);
-	
     );
 }
 
