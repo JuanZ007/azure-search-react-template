927f295d607d0f690c196606fa9bf693a49bf1bb
JuanZ007 <32806138+JuanZ007@users.noreply.github.com>
2021-09-21 21:05:11 -0500

    Update Result.js

--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -4,10 +4,9 @@ import './Result.css';
 
 export default function Result(props) {
     return(
+	<a href="http://{`${props.document.location}`}" target="_blank">props.document.text</a>
 
-	<a href="http://{`${props.document.location}`}" target="_blank">
-
-	 console.log(props.document);
+	console.log(props.document);
 	console.log(props.document.text);
 	console.log(props.document.location);
 	
