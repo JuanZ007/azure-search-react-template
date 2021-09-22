815ec080dc4b4f26e8dbbfa8d00d9f09b49f2434
JuanZ007 <32806138+JuanZ007@users.noreply.github.com>
2021-09-21 19:14:33 -0500

    Update Result.js

--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -4,6 +4,7 @@ import './Result.css';
 
 export default function Result(props) {
     return(
+        <h6 className="title-style">{props.document.text} - {props.document.title} </h6>
     <div className="card result">
         <a href={`/details/${props.document.id}`}>
             <img className="card-img-top" src={props.document.image_url} alt={props.document.original_title}></img>
