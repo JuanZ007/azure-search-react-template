1e11b8af55052a635fecff87d137000695f11859
JuanZ007 <32806138+JuanZ007@users.noreply.github.com>
2021-09-21 20:56:35 -0500

    Update Result.js

--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -11,14 +11,7 @@ export default function Result(props) {
 	console.log(props.document.text);
 	console.log(props.document.location);
 	
-	//<div className="card result">
-    //    <a href={`/details/${props.document.id}`}>
-    //        <img className="card-img-top" src={props.document.image_url} alt={props.document.original_title}></img>
-    //        <div className="card-body">
-    //            <h6 className="title-style">{props.document.original_title}</h6>
-    //        </div>
-    //    </a>
-    //</div>
+
     );
 }
 
