a9f96bb195574aedcff59be3252b6064c06fb163
JuanZ007 <32806138+JuanZ007@users.noreply.github.com>
2021-09-29 07:51:09 -0500

    Update Result.js

--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -1,12 +1,36 @@
 import React from 'react';
-
+import { Text } from 'react-native';
 import './Result.css';
 
 export default function Result(props) {
+    const string = {props.document.text}
+    const length = 50
     return(
 	    	<div>
 	    		<a href={`http://${props.document.location}`}><h6 className="title-style">{props.document.title}</h6></a>
-			{props.document.text}
+			
+			// Split text into individual words and count length
+			  const words = string.split(' ');
+			  const count = words.length;
+
+			  // Prepare elements and position tracker
+			  const elements = [];
+			  let position = 0;
+
+			  // Loop through words whilst position is less than count
+			  while (position < count) {
+			    // Prepare text for specified length and increment position
+			    const text = words.slice(position, length).join(' ');
+			    position += length;
+
+			    // Append text element
+			    elements.push((
+			      <Text>{text}</Text>
+			    ));
+			  }
+			
+			
+			
 			<br></br>
 		</div>
     );
