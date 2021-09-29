ee4c92f469efcaf575ccd106122b767398fed154
JuanZ007 <32806138+JuanZ007@users.noreply.github.com>
2021-09-29 11:14:18 -0500

    Update Result.js

--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -9,7 +9,7 @@ export default function Result(props) {
     const words = text.split(' ');
     const count = words.length;
     const elements = [];
-    
+    let tagHtml = ""
     let position = 0;
     while (position < count) {
 			    const text = words.slice(position, length).join(' ');
@@ -18,9 +18,32 @@ export default function Result(props) {
 			      text
 			    ));
 			  }
+    if (location.includes('cloudoperations')) { 
+       tagHtml = "Operations";      
+    }
+    if (location.includes('bizagidocumentation')) { 
+       tagHtml = "Technical";      
+    }
+    if (location.includes('bizagiknowledgebase')) { 
+       tagHtml = "KB internal";      
+    }
+    if (location.includes('cs-bizagi')) { 
+       tagHtml = "Tickets";      
+    }
+    if (location.includes('kb.bizagi.com')) { 
+       tagHtml = "KB External";      
+    }
+    if (location.includes('bizagidevdocs')) { 
+       tagHtml = "Bizagi Devs";      
+    }
+	
+	
+	
+	
+	
     return(
 	    	<div>
-	    		<a href={`http://${location}`}><h6 className="title-style">{title}</h6></a>
+	    		<a href={`http://${location}`}><h6 className="title-style">{tagHtml}: {title}</h6></a>
 		 	{elements};
 			<br></br>
 		</div>
