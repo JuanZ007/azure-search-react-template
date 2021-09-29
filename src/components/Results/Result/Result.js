0500af975deb9b2a14a3a314a459c159139dc955
JuanZ007 <32806138+JuanZ007@users.noreply.github.com>
2021-09-29 08:46:14 -0500

    Update Result.js

--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -3,35 +3,29 @@ import { Text } from 'react-native';
 import './Result.css';
 
 export default function Result(props) {
-    const length = 50;
-    return(
-	    	<div>
-	    		<a href={`http://${props.document.location}`}><h6 className="title-style">{props.document.title}</h6></a>
-			
-			// Split text into individual words and count length
-			  const words = {props.document.text}.split(' ');
-			  const count = words.length;
-
-			  // Prepare elements and position tracker
-			  const elements = [];
-			  let position = 0;
-
-			  // Loop through words whilst position is less than count
-			  while (position < count) {
-			    // Prepare text for specified length and increment position
+    const length = 50; 
+    const location = {props.document.location}
+    const title= {props.document.title}
+    const text = {props.document.text}
+    const words = text.split(' ');
+    const count = words.length;
+    const elements = [];
+    
+    let position = 0;
+    while (position < count) {
 			    const text = words.slice(position, length).join(' ');
 			    position += length;
-
-			    // Append text element
 			    elements.push((
 			      <Text>{text}</Text>
 			    ));
 			  }
-			
-			
-			
+    return(
+	    	<div>
+	    		<a href={`http://${location}`}><h6 className="title-style">{title}</h6></a>
+		 	{elements};
 			<br></br>
 		</div>
     );
-}
-
+}	
+	
+	
