90a758b9bee757e548bea1bd10c912c701ecf726
JuanZ007 <32806138+JuanZ007@users.noreply.github.com>
2021-09-29 11:30:41 -0500

    Update Result.js

--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -10,6 +10,7 @@ export default function Result(props) {
     const count = words.length;
     const elements = [];
     let tagHtml = ""
+    let cssStyle = ""
     let position = 0;
     while (position < count) {
 			    const text = words.slice(position, length).join(' ');
@@ -19,22 +20,28 @@ export default function Result(props) {
 			    ));
 			  }
     if (location.includes('cloudoperations')) { 
-       tagHtml = "Operations";      
+       tagHtml = "Operations";
+       cssStyle = "tag1";	    
     }
     if (location.includes('bizagidocumentation')) { 
        tagHtml = "Technical";      
+       cssStyle = "tag2";	    	    
     }
     if (location.includes('bizagiknowledgebase')) { 
-       tagHtml = "KB internal";      
+       tagHtml = "KB internal";     
+       cssStyle = "stylekbinternal";	    	    
     }
     if (location.includes('cs-bizagi')) { 
        tagHtml = "Tickets";      
+       cssStyle = "styleticket";	    	    
     }
     if (location.includes('kb.bizagi.com')) { 
-       tagHtml = "KB External";      
+       tagHtml = "KB External";     
+       cssStyle = "stylekbexternal";	    	    
     }
     if (location.includes('bizagidevdocs')) { 
-       tagHtml = "Bizagi Devs";      
+       tagHtml = "Bizagi Devs";     
+       cssStyle = "styleBizagiDevs";	    	    
     }
 	
 	
@@ -43,7 +50,7 @@ export default function Result(props) {
 	
     return(
 	    	<div>
-	    		<label className="tag">{tagHtml}</label>: <a href={`http://${location}`}><h6 className="title-style">{title}</h6></a>
+	    		<label className="tag {cssStyle}">{tagHtml}</label> <a href={`http://${location}`}><h6 className="title-style">{title}</h6></a>
 		 	{elements};
 			<br></br>
 		</div>
