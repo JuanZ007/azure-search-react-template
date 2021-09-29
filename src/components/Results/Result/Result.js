98d5f0311a21199eeaf7b7a74c0f10124124fc76
JuanZ007 <32806138+JuanZ007@users.noreply.github.com>
2021-09-29 00:09:08 -0500

    Update Result.js

--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -5,6 +5,7 @@ import './Result.css';
 export default function Result(props) {
     return(
 	    	<div>
+	    		<h6 className="title-style">{props.document.title}</h6>
 			<a href={`http://${props.document.location}`} target="_blank">{props.document.text}</a>
 		</div>
     );
