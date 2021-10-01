154481906203073b926cc026d136edb0d820ee25
JuanZ007 <32806138+JuanZ007@users.noreply.github.com>
2021-10-01 14:19:05 -0500

    Update index.js

--- api/Suggest/index.js
+++ api/Suggest/index.js
@@ -21,10 +21,10 @@ module.exports = async function (context, req) {
     const suggester = (req.query.suggester || (req.body && req.body.suggester));
     
     // Let's get the top 5 suggestions for that search term
-    const suggestions = await client.suggest(q, suggester, {top: parseInt(top)});
+    //const suggestions = await client.suggest(q, suggester, {top: parseInt(top)});
     //const suggestions = await client.autocomplete(q, suggester, {top: parseInt(top)});
 
-    context.log(suggestions);
+    context.log(suggester);
 
     context.res = {
         // status: 200, /* Defaults to 200 */
